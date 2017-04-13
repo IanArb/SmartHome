d981216d37bd630e4eeaa9c60acb5eb91e2c148e
Ian Arbuckle <ian.arbuckle18@gmail.com>
2017-04-14 00:07:18 +0100

    - Importing Gson library - Setting up json messaging format

--- src/main/java/utils/UIConstants.java
+++ src/main/java/utils/UIConstants.java
@@ -5,7 +5,7 @@ public class UIConstants {
     public static final int UIWIDTH = 550;
     public static final int UIHEIGHT = 600;
     public static final int COMPONENTWIDTH = 520;
-    public static final int CONTROLY = 425;
+    public static final int CONTROLY = 365;
 
     public static final String WARM_BUTTON = "Warm room";
     public static final String LIGHTS_LABEL = "Lights: ON/OFF";
