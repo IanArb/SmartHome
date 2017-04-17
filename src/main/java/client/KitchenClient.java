1d056a75f6210245fb955b9373c9ccb1db3da3bf
firminofranciele <firminfranciele@gmail.com>
2017-04-17 13:49:23 +0100

    Json Messaging between Kitchen Service/Client

--- src/main/java/client/KitchenClient.java
+++ src/main/java/client/KitchenClient.java
@@ -1,21 +1,24 @@
 package client;
 
 import clientui.KitchenUI;
+import models.KitchenModel;
 import utils.Constants;
 
 /**
  * Created by Fran Firmino on 11/04/2017.
  */
 public class KitchenClient extends Client {
-    private boolean isBoiling= false;
+    private boolean isBoiling = false;
     private boolean isOvenOn = false;
     private boolean isTapOn = false;
+    private KitchenModel model;
 
     public KitchenClient() {
         super();
         serviceType = Constants.UDP_SOCKET_KITCHEN;
         ui = new KitchenUI(this);
         name = "Kitchen";
+        model = new KitchenModel();
     }
 
     /**
@@ -23,11 +26,12 @@ public class KitchenClient extends Client {
      */
     public void boil() {
         if (!isBoiling) {
-            String action = sendMessage(Constants.KETTLE_BOIL_REQUEST);
-            if (action.equals(Constants.REQUEST_OK)) {
-                isBoiling = true;
-                ui.updateArea("Kettle is boiling");
-            }
+            model.setRequest(Constants.REQUEST_OK);
+            model.setBoilKettle(Constants.KETTLE_BOIL_REQUEST);
+            sendMessage(model.getBoilKettle());
+            ui.updateArea("Kettle is boiling");
+            isBoiling = true;
+
         } else {
             ui.updateArea("Kettle is already boiling");
         }
@@ -39,56 +43,65 @@ public class KitchenClient extends Client {
 
     public void oven() {
         if (!isOvenOn) {
-            String action = sendMessage(Constants.OVEN_ON_REQUEST);
-            if (action.equals(Constants.REQUEST_OK)) {
-                isOvenOn = true;
-                ui.updateArea("Oven is on");
-            }
+            model.setRequest(Constants.REQUEST_OK);
+            model.setOvenSwitch(Constants.OVEN_ON_REQUEST);
+            sendMessage(model.getOvenSwitch());
+            isOvenOn = true;
+            ui.updateArea("Oven is on");
         } else {
-            sendMessage(Constants.OVEN_OFF_REQUEST);
+            model.setRequest(Constants.REQUEST_OK);
+            model.setOvenSwitch(Constants.OVEN_OFF_REQUEST);
+            sendMessage(model.getOvenSwitch());
             isOvenOn = false;
             ui.updateArea("Oven is off");
         }
     }
 
+    /**
+     * sends a message to turn tap on/off.
+     */
+
     public void tap() {
-        if(!isTapOn) {
-            String action = sendMessage(Constants.TAP_ON_REQUEST);
-            if(action.equals(Constants.REQUEST_OK)) {
-                isTapOn = true;
-                ui.updateArea("Tap is turned on");
-            }
+        if (!isTapOn) {
+            model.setTapSwitch(Constants.TAP_ON_REQUEST);
+            model.setRequest(Constants.REQUEST_OK);
+            sendMessage(model.getTapSwitch());
+            isTapOn = true;
+            ui.updateArea("Tap is on");
+
         } else {
-            sendMessage(Constants.TAP_OFF_REQUEST);
+            model.setTapSwitch(Constants.LAMP_OFF_REQUEST);
+            model.setRequest(Constants.REQUEST_OK);
+            sendMessage(model.getTapSwitch());
             isTapOn = false;
-            ui.updateArea("Tap is turned off");
+            ui.updateArea("Tap is off");
         }
     }
 
-        @Override
-        public void updatePoll(String msg) {
-            switch (msg) {
-                case "Kettle is 100% boiled.":
-                    isBoiling = false;
-                    break;
-                default:
-                    isBoiling = false;
-                    isOvenOn = false;
-                    isTapOn = false;
-                    break;
-            }
-        }
-
-        @Override
-        public void disable() {
-            super.disable();
-            ui = new KitchenUI(this);
-            isBoiling = false;
-            isOvenOn = false;
-            isTapOn = false;
-
+    @Override
+    public void updatePoll(String msg) {
+        switch (msg) {
+            case "Kettle is 100% boiled.":
+                isBoiling = false;
+                break;
+            default:
+                isBoiling = false;
+                isOvenOn = false;
+                isTapOn = false;
+                break;
         }
     }
+
+    @Override
+    public void disable() {
+        super.disable();
+        ui = new KitchenUI(this);
+        isBoiling = false;
+        isOvenOn = false;
+        isTapOn = false;
+
+    }
+}
 
 
 
